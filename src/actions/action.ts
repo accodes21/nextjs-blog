a2296f3f7e57b070d17350ec2310bc91ee0ba070
Aarya Chopkar <aaryachopkar@gmail.com>
2024-07-03 23:28:02 +0530

    prisma initialized

---
+++ src/actions/action.ts
@@ -0,0 +1,16 @@
+"use server"
+
+import prisma from "@/lib/db";
+
+export async function createPost(formData: FormData) {
+    const title = formData.get("title") as string
+    const body = formData.get("body") as string
+
+    //update database
+    await prisma.post.create({
+        data:{
+            title,
+            body
+        }
+    })
+}
