e932fea53a08d4163b711ca168070fead57420c7
Aarya Chopkar <aaryachopkar@gmail.com>
2024-07-09 16:29:43 +0530

    updated styling

--- src/firebase/SignIn.tsx
+++ src/firebase/SignIn.tsx
@@ -4,7 +4,9 @@ import { signInWithGoogle } from '@/firebase/firebase';
 export default function SignIn() {
   return (
     <div className='text-center pt-16 px-5'>
-      <button className='sign-out' onClick={signInWithGoogle}>Sign in with Google</button>
+      <button className='sign-in' onClick={signInWithGoogle}>
+        <span className='sign-span'>Sign in with Google</span>
+      </button>
       <p className='sign-p'>Sign in to create post with title and body.</p>
     </div>
   );
