53d4fd2281643042592630317313d743579e88a4
Aarya Chopkar <aaryachopkar@gmail.com>
2024-07-22 23:35:45 +0530

    feat: added zod for schema validation

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -2,19 +2,35 @@
 
 import prisma from "@/lib/db";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const postSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  body: z.string().min(1, "Body is required"),
+});
 
 export async function createPost(formData: FormData) {
   const title = formData.get("title") as string;
   const body = formData.get("body") as string;
 
+  const result = postSchema.safeParse({ title, body });
+
+  if (!result.success) {
+    const errors = result.error.format();
+    console.error("Validation errors:", errors);
+    return; // Handle errors appropriately
+  }
+
   //update database
   await prisma.post.create({
     data: {
-      title,
-      body,
+      title: result.data.title,
+      body: result.data.body,
     },
   });
 
   //revalidate /posts to instantly load new post
   revalidatePath("/posts");
+  redirect("/posts");
 }
